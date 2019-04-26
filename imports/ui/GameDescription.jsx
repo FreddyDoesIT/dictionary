58e456202d157d309533017972b5544b8595d1e6
QiminCao <sevenscao@gmail.com>
2019-04-26 06:15:37 -0700

    Update challenge description

--- imports/ui/GameDescription.jsx
+++ imports/ui/GameDescription.jsx
@@ -8,22 +8,34 @@ export default class GameDescription extends React.Component {
 				<Message.Header>How to Chanllenge Yourself</Message.Header>
 				<Message.List>
 					<Message.Item>
-						Click play button to start challenging yourself
+						Click play Go to Challenge button to challenge yourself. 
+						If Your list has less than 10 words. You need to add more to your list to join the challenge game.
 					</Message.Item>
 					<Message.Item>
-						You will be randomly assigned an opponent
+						Once you are on the Challenge Game page, click Play button to start a new game or join a existing game. You will be randomly assigned an opponent.
 					</Message.Item>
 					<Message.Item>
-						You will be given 10 words picked randomly from all
-						searching records
+						You and your opponent will be given 10 words questions picked randomly from all searching records
 					</Message.Item>
 					<Message.Item>
-						Game is over the moment whoever finishes to answer the
-						questions
+						Once you select an answer, you cannot change to other answers. 
+						After selecting, if the answer background color turns to red, that means you chose wrong.
+						If it is green, then you chose right.
 					</Message.Item>
 					<Message.Item>
-						Every word is worth 10 points. The winner will be
-						awarded 100 points
+						Every question requires answer for you and your opponent. 
+						Once you and your opponent answered the question, it will shows you next question. 
+					</Message.Item>
+
+					<Message.Item>
+						Every word is worth 10 points. You can check the scoreboard on the right side.
+					</Message.Item>
+					<Message.Item>
+						Once ten questions are finished, there will be a message shows {"You win"} or {"You lost"} or {"Tie"} according to the points of you and your opponent.
+					</Message.Item>
+
+					<Message.Item>
+						If anyone is logged out during the game, then the opponent will be the winner!
 					</Message.Item>
 				</Message.List>
 			</Message>
